298a3b8a7ec05a545f992e12add83d0535e8898b
erika-n <erika.nesse@gmail.com>
2021-03-09 13:45:27 -0500

    Still debugging mobile width

--- simple_harmonic_oscillator.js
+++ simple_harmonic_oscillator.js
@@ -21,9 +21,9 @@ values = [];
 n_values = 500;
 
 function setup(){
-  w = 400;
-  if(window.innerWidth < w){
-    w = 0.8*window.innerWidth; //0.8*window.innerWidth;
+  w = 500;
+  if(window.outerWidth < w){
+    w = 0.8*window.outerWidth; //0.8*window.innerWidth;
     
   }
   h = 3.0*w/4.0;
